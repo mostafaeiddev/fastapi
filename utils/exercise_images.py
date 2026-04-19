aa9946c7d180e6967f18cbea8dd0c04f40271a45
mostafaeiddev <mostafa.eid.dev@gmail.com>
2026-04-19 22:48:45 +0200

    change the directory name in cloudinary

--- utils/exercise_images.py
+++ utils/exercise_images.py
@@ -2,29 +2,29 @@ import re
 
 # ✅ حط كل الصور هنا
 CLOUDINARY_MAP = {
-    "barbell bench press": "https://res.cloudinary.com/duprntyar/image/upload/v1776339493/moveon/workouts/Close%2BGrip%2BBarbell%2BBench%2BPress.webp",
-    "dumbbell bench press": "https://res.cloudinary.com/duprntyar/image/upload/v1776339498/moveon/workouts/dumbbell-bench-press.jpg.webp",
-    "machine bench press": "https://res.cloudinary.com/duprntyar/image/upload/v1776339512/moveon/workouts/Machine%20Bench%20Press.webp",
-    "rope pushdowns": "https://res.cloudinary.com/duprntyar/image/upload/v1776339518/moveon/workouts/Rope%20Pushdowns.webp",
-    "lat pulldowns": "https://res.cloudinary.com/duprntyar/image/upload/v1776339507/moveon/workouts/Lat%20Pulldowns.webp",
-    "dumbbell rows": "https://res.cloudinary.com/duprntyar/image/upload/v1776339497/moveon/workouts/Dumbbell%20Rows.webp",
-    "seated rows close-grip": "https://res.cloudinary.com/duprntyar/image/upload/v1776339523/moveon/workouts/Seated%20Rows%20Close-Grip.webp",
-    "cable curls": "https://res.cloudinary.com/duprntyar/image/upload/v1776339491/moveon/workouts/Cable%20Curls.webp",
-    "incline dumbbell curls": "https://res.cloudinary.com/duprntyar/image/upload/v1776339505/moveon/workouts/Incline%20Dumbbell%20Curls.webp",
-    "barbell rows": "https://res.cloudinary.com/duprntyar/image/upload/v1776339488/moveon/workouts/Barbell%20Rows.webp",
-    "machine shoulder press": "https://res.cloudinary.com/duprntyar/image/upload/v1776339513/moveon/workouts/Machine%20Shoulder%20Press.webp",
-    "face pulls": "https://res.cloudinary.com/duprntyar/image/upload/v1776339500/moveon/workouts/Face%20Pulls.webp",
-    "cable lateral raises": "https://res.cloudinary.com/duprntyar/image/upload/v1776339491/moveon/workouts/Cable%20Lateral%20Raises.webp",
-    "dumbbell lunges": "https://res.cloudinary.com/duprntyar/image/upload/v1776339511/moveon/workouts/Lunges.webp",
-    "lying leg curl": "https://res.cloudinary.com/duprntyar/image/upload/v1776339512/moveon/workouts/Lying%20Leg%20Curl.webp",
-    "leg extensions": "https://res.cloudinary.com/duprntyar/image/upload/v1776339508/moveon/workouts/Leg%20Extensions.webp",
-    "seated calf raises": "https://res.cloudinary.com/duprntyar/image/upload/v1776339521/moveon/workouts/Seated%20Calf%20Raises.webp",
-    "hip adductor machine": "https://res.cloudinary.com/duprntyar/image/upload/v1776339504/moveon/workouts/Hip%20Adductor%20Machine.webp",
-    "romanian deadlift": "https://res.cloudinary.com/duprntyar/image/upload/v1776339517/moveon/workouts/Romani%20Deadleft.webp",
+    "barbell bench press": "https://res.cloudinary.com/duprntyar/image/upload/v1776339493/moveon/Final_Images/Close%2BGrip%2BBarbell%2BBench%2BPress.webp",
+    "dumbbell bench press": "https://res.cloudinary.com/duprntyar/image/upload/v1776339498/moveon/Final_Images/dumbbell-bench-press.jpg.webp",
+    "machine bench press": "https://res.cloudinary.com/duprntyar/image/upload/v1776339512/moveon/Final_Images/Machine%20Bench%20Press.webp",
+    "rope pushdowns": "https://res.cloudinary.com/duprntyar/image/upload/v1776339518/moveon/Final_Images/Rope%20Pushdowns.webp",
+    "lat pulldowns": "https://res.cloudinary.com/duprntyar/image/upload/v1776339507/moveon/Final_Images/Lat%20Pulldowns.webp",
+    "dumbbell rows": "https://res.cloudinary.com/duprntyar/image/upload/v1776339497/moveon/Final_Images/Dumbbell%20Rows.webp",
+    "seated rows close-grip": "https://res.cloudinary.com/duprntyar/image/upload/v1776339523/moveon/Final_Images/Seated%20Rows%20Close-Grip.webp",
+    "cable curls": "https://res.cloudinary.com/duprntyar/image/upload/v1776339491/moveon/Final_Images/Cable%20Curls.webp",
+    "incline dumbbell curls": "https://res.cloudinary.com/duprntyar/image/upload/v1776339505/moveon/Final_Images/Incline%20Dumbbell%20Curls.webp",
+    "barbell rows": "https://res.cloudinary.com/duprntyar/image/upload/v1776339488/moveon/Final_Images/Barbell%20Rows.webp",
+    "machine shoulder press": "https://res.cloudinary.com/duprntyar/image/upload/v1776339513/moveon/Final_Images/Machine%20Shoulder%20Press.webp",
+    "face pulls": "https://res.cloudinary.com/duprntyar/image/upload/v1776339500/moveon/Final_Images/Face%20Pulls.webp",
+    "cable lateral raises": "https://res.cloudinary.com/duprntyar/image/upload/v1776339491/moveon/Final_Images/Cable%20Lateral%20Raises.webp",
+    "dumbbell lunges": "https://res.cloudinary.com/duprntyar/image/upload/v1776339511/moveon/Final_Images/Lunges.webp",
+    "lying leg curl": "https://res.cloudinary.com/duprntyar/image/upload/v1776339512/moveon/Final_Images/Lying%20Leg%20Curl.webp",
+    "leg extensions": "https://res.cloudinary.com/duprntyar/image/upload/v1776339508/moveon/Final_Images/Leg%20Extensions.webp",
+    "seated calf raises": "https://res.cloudinary.com/duprntyar/image/upload/v1776339521/moveon/Final_Images/Seated%20Calf%20Raises.webp",
+    "hip adductor machine": "https://res.cloudinary.com/duprntyar/image/upload/v1776339504/moveon/Final_Images/Hip%20Adductor%20Machine.webp",
+    "romanian deadlift": "https://res.cloudinary.com/duprntyar/image/upload/v1776339517/moveon/Final_Images/Romani%20Deadleft.webp",
 }
 
 # fallback لو مش لاقي
-DEFAULT_IMAGE = "https://res.cloudinary.com/duprntyar/image/upload/v1776339512/moveon/workouts/Machine%20Bench%20Press.webp"
+DEFAULT_IMAGE = "https://res.cloudinary.com/duprntyar/image/upload/v1776339512/moveon/Final_Images/Machine%20Bench%20Press.webp"
 
 
 def normalize(text: str):
